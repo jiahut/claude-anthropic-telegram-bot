6532a0211084a385c023cce2eeb4c7593f6df6aa
zhijia.zhang <zhijia.zhang@transsion.com>
2024-11-30 10:40:56 +0800

    fix menu

--- bot.py
+++ bot.py
@@ -380,10 +380,8 @@ def main():
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     application.add_handler(CallbackQueryHandler(button))
     application.add_error_handler(error_handler)
-    application.run_polling(poll_interval=1.0, timeout=30)
-
-  # Set bot commands
     asyncio.run(set_commands(application))
+    application.run_polling(poll_interval=1.0, timeout=30)
 
 if __name__ == '__main__':
     main()
